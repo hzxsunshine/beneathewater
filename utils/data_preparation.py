5ea025a39b1f1a1f3da5dc29e6b2163b7481c0e7
Zixuan Hu <hzxspro@gmail.com>
2023-11-15 17:01:42 -0500

    updated google drive link to images, added reference code

--- utils/data_preparation.py
+++ utils/data_preparation.py
@@ -5,10 +5,10 @@ from os import subprocess
 # Load data
 def get_uieb_data():
     base_url = "https://drive.google.com/uc?id="
-    id_ = "1ylAgljMIMq8zIwTiU4w3SrUlGQ3sxs-U"
+    id_ = "1Z5LKgmZmFRCQ6kZzHWTcV8gzeLcxHtXE"
     url = base_url + id_
     gdown.download(url)
-    subprocess.run(["tar", "-xvf", "./uieb.tar.gz"])
+    subprocess.run(["tar", "-xvf", "./uieb_dataset.tar.gz"])
 
 def setup_colab_env():
     subprocess.run(["pip", "install", "ftfy", "regex", "tqdm"])
