2ea11adfe194636998182462f4b9801685217fe0
Zoeeww <145498387+Zoeeww@users.noreply.github.com>
2023-11-21 17:35:04 -0500

    constants.py

--- configs/constants.py
+++ configs/constants.py
@@ -2,7 +2,25 @@ from typing import List, Final
 
 path_to_data: Final[str] = "./uieb_dataset"
 contrastive_pairs: Final[List[List[str]]] = [
-    ["Accurate Color Representation", "Blue/Green Color Cast"], # "Color representation is accurate"
+    ["Accurate Color Representation", "Blue/Green Color Cast"],
+    ["Backlit Subjects", "Frontlit Subjects"], 
+    ["Soft Focus", "Sharp Detail"],
+    ["Wide Angle", "Macro"],
+    ["Underexposed", "Overexposed"], 
+    ["Shallow Depth of Field", "Deep Depth of Field"],
+    ["High ISO Noise", "Low ISO Clarity"],
+    ["Filtered Light", "Direct Light"],
+    ["Dynamic Movement", "Static Calm"],
+    ["Clarity", "Murkiness"],
+    ["Textured Surfaces", "Smooth Surfaces"],
+    ["Top-Down View", "Eye-Level View"],
+    ["Natural Ambient Light", "Artificial Light"], 
+    ["Unaltered Colors", "Color Enhanced"],
+    ["Organic Shapes", "Geometric Structures"],
+    ["Silhouetted", "Illuminated Subjects"],
+    ["Foreground Interest", "Background Dominance"],
+    
+    # "Color representation is accurate"
 ]
 
 def select_contrastive_pairs(indices=None):
